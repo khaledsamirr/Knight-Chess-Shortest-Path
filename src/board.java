9eccd7f3295d962bfcaa5dddde27b7c94ad98d6c
Youssef Ahmed <youssef9300@hotmail.com>
2021-06-09 20:18:00 +0200

    Addition of human Interaction

--- src/board.java
+++ src/board.java
@@ -3,112 +3,117 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class cell{
- int x, y,distance;
- public cell(int x,int y){
-  this.x=x;
-  this.y=y;
-  this.distance=0;
- }
- public cell(int x,int y,int d){
-        this.x=x;
-        this.y=y;
-        this.distance=d;
+class cell {
+    int x, y, distance;
+
+    public cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.distance = 0;
+    }
+
+    public cell(int x, int y, int d) {
+        this.x = x;
+        this.y = y;
+        this.distance = d;
     }
 }
 
 public class board {
- int N;
- int [] KnightPos;
- int [] goal;
- int [][] obstacles;
- int [][] board;
- boolean [][] full;
- ArrayList<cell>path=new ArrayList<cell>();
- ArrayList<cell>solution=new ArrayList<cell>();
- ArrayList<cell>cells=new ArrayList<cell>();
+    int N;
+    int[] KnightPos;
+    int[] goal;
+    int[][] obstacles;
+    int[][] board;
+    boolean[][] full;
+    ArrayList<cell> path = new ArrayList<cell>();
+    ArrayList<cell> solution = new ArrayList<cell>();
+    ArrayList<cell> cells = new ArrayList<cell>();
 
- public board(int n, int[] k,int[]g,int[][] o){
-   this.N=n;
-   this.KnightPos=k;
-   this.goal=g;
-   this.obstacles=o;
- }
- public void initalizeBoard(){
-      board=new int [N][N];
-      full= new boolean[N+1][N+1];
-      for (int i = 0; i < N; i++) {
-       for (int j = 0; j < N; j++)
-          full[i][j] = false;
-      }
-      int x,y;
-      for(int i=0;i<obstacles.length;i++){
-         x=obstacles[i][0];
-         y=obstacles[i][1];
-         full[x][y]=true;
-      }
-      full[KnightPos[0]][KnightPos[1]]=true;
- }
- public boolean isCell(int x, int y){
-        if( x < 0 || y < 0 || x > this.N || y > this.N){
+    public board(int n, int[] k, int[] g, int[][] o) {
+        this.N = n;
+        this.KnightPos = k;
+        this.goal = g;
+        this.obstacles = o;
+    }
+
+    public void initalizeBoard() {
+        board = new int[N][N];
+        full = new boolean[N + 1][N + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                full[i][j] = false;
+        }
+        int x, y;
+        for (int i = 0; i < obstacles.length; i++) {
+            x = obstacles[i][0];
+            y = obstacles[i][1];
+            full[x][y] = true;
+        }
+        full[KnightPos[0]][KnightPos[1]] = true;
+    }
+
+    public boolean isCell(int x, int y) {
+        if (x < 0 || y < 0 || x > this.N || y > this.N) {
             return false;
         }
         return true;
     }
- public void reachGoal(){
-     int NumOfMoves=0;
-     int KnightX[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
-     int KnightY[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
-     cell c;
-     path.add(new cell(KnightPos[0], KnightPos[1]));
-     while (!path.isEmpty()) {
-         c = path.get(0);
-         path.remove(0);
-         if (c.x == goal[0] && c.y ==goal[1]) {
-             NumOfMoves=c.distance;
-             break;
-         }
-         int x1,y1;
-         for(int i=0;i<KnightX.length;i++) {
-             x1 = c.x + KnightX[i];
-             y1 = c.y + KnightY[i];
 
-             if (isCell(x1, y1) && !full[x1][y1]) {
-                 full[x1][y1] = true;
-                 path.add(new cell(x1, y1, c.distance + 1));
-                 solution.add(new cell(x1, y1, c.distance + 1));
-             }
-         }
+    public void reachGoal() {
+        int NumOfMoves = 0;
+        int KnightX[] = {-2, -1, 1, 2, -2, -1, 1, 2};
+        int KnightY[] = {-1, -2, -2, -1, 1, 2, 2, 1};
+        cell c;
+        path.add(new cell(KnightPos[0], KnightPos[1]));
+        while (!path.isEmpty()) {
+            c = path.get(0);
+            path.remove(0);
+            if (c.x == goal[0] && c.y == goal[1]) {
+                NumOfMoves = c.distance;
+                break;
+            }
+            int x1, y1;
+            for (int i = 0; i < KnightX.length; i++) {
+                x1 = c.x + KnightX[i];
+                y1 = c.y + KnightY[i];
+
+                if (isCell(x1, y1) && !full[x1][y1]) {
+                    full[x1][y1] = true;
+                    path.add(new cell(x1, y1, c.distance + 1));
+                    solution.add(new cell(x1, y1, c.distance + 1));
+                }
+            }
+        }
+        int min;
+        int minsum;
+        cell find = solution.get(0);
+        int sum = 0, sum2 = 0;
+        for (int i = 1; i <= NumOfMoves; i++) {
+            min = (goal[0] - solution.get(0).x) + (goal[1] - solution.get(0).y);
+            minsum = Math.abs(solution.get(0).x - solution.get(0).y);
+            for (int j = 1; j < solution.size(); j++) {
+                if (solution.get(j).distance == i) {
+                    sum = (goal[0] - solution.get(j).x) + (goal[1] - solution.get(j).y);
+                    sum2 = Math.abs(solution.get(j).x - solution.get(j).y);
+                    if (sum <= min && sum2 <= minsum) {
+                        min = sum;
+                        minsum = sum2;
+                        find = solution.get(j);
+                    }
+                }
+
+            }
+            cells.add(find);
+        }
+        if (NumOfMoves > 0) {
+            System.out.println("Number of moves: " + NumOfMoves);
+            System.out.println("____________________________________");
+            for (int i = 0; i < cells.size(); i++) {
+                System.out.println("Move #" + (i + 1) + ": (" + cells.get(i).x + "," + cells.get(i).y + ").");
+            }
+        } else {
+            System.out.println("can't solve");
+        }
     }
-     int min;
-     int minsum;
-     cell find=solution.get(0);
-     int sum=0,sum2=0;
-     for(int i=1;i<=NumOfMoves;i++){
-         min=(goal[0]-solution.get(0).x)+(goal[1]-solution.get(0).y);
-         minsum=Math.abs(solution.get(0).x-solution.get(0).y);
-         for(int j=1;j<solution.size();j++){
-             if(solution.get(j).distance==i){
-                 sum=(goal[0]-solution.get(j).x)+(goal[1]-solution.get(j).y);
-                 sum2=Math.abs(solution.get(j).x-solution.get(j).y);
-                 if(sum<=min&&sum2<=minsum) {
-                     min=sum;
-                     minsum=sum2;
-                     find = solution.get(j);
-                 }
-             }
-
-         }
-         cells.add(find);
-     }
-     if(NumOfMoves>0){
-         System.out.println("Number of moves: "+NumOfMoves);
-         System.out.println("____________________________________");
-         for(int i=0;i<cells.size();i++){
-             System.out.println("Move #"+(i+1)+": ("+cells.get(i).x+","+cells.get(i).y+").");
-         }
-    }else{
-         System.out.println("can't solve");
-     }
-}
 }
--- src/main.java
+++ src/main.java
@@ -1,10 +1,73 @@
+import java.util.Scanner;
+
 public class main {
 
-    public static void main (String args[]){
-        int knightPos[] = { 0, 0 };
-        int GoalPos[] = { 6, 6 };
-        int[][] obstacles={{1,4} , {2,2}, {6,2}, {4,4},{5,1},{3,7}};
-        board b=new board(8,knightPos,GoalPos,obstacles);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int knightPos[] = {0, 0};
+        int GoalPos[] = {0, 0};
+        int[][] obstacles;
+
+        int num = 0;
+
+        String[] temp;
+        System.out.println("Please Enter position of knight: \n" +
+                "Example X Y");
+        temp = sc.nextLine().split(" ");
+
+        while (Integer.valueOf(temp[0]) > 7 || Integer.valueOf(temp[1]) > 7
+                || Integer.valueOf(temp[0]) < 0 || Integer.valueOf(temp[1]) < 0) {
+            System.out.println("Invalid Input in the Position of the Knight");
+            System.out.println("Please Enter position of knight: \n" +
+                    "Example X Y");
+            temp = sc.nextLine().split(" ");
+        }
+
+        knightPos[0] = Integer.valueOf(temp[0]);
+        knightPos[1] = Integer.valueOf(temp[1]);
+
+
+        System.out.println("Please Enter position of Goal: \n" +
+                "Example X Y");
+        temp = sc.nextLine().split(" ");
+
+        while (Integer.valueOf(temp[0]) > 7 || Integer.valueOf(temp[1]) > 7
+                || Integer.valueOf(temp[0]) < 0 || Integer.valueOf(temp[1]) < 0) {
+            System.out.println("Invalid Input in the Position of the Goal Title");
+            System.out.println("Please Enter position of Goal: \n" +
+                    "Example X Y");
+            temp = sc.nextLine().split(" ");
+        }
+
+        GoalPos[0] = Integer.valueOf(temp[0]);
+        GoalPos[1] = Integer.valueOf(temp[1]);
+
+        System.out.print("Please Enter how many obstacles: ");
+        num = Integer.valueOf(sc.nextLine());
+
+        obstacles = new int[num][2];
+
+
+        for (int i = 0; i < num; i++) {
+
+            System.out.println("Please Enter where is the obstacle number " + (i + 1) +
+                    "\nExample X Y");
+
+            temp = sc.nextLine().split(" ");
+
+            while (Integer.valueOf(temp[0]) > 7 || Integer.valueOf(temp[1]) > 7
+                    || Integer.valueOf(temp[0]) < 0 || Integer.valueOf(temp[1]) < 0) {
+                System.out.println("Invalid Input in the Position of the Obstacle number " + (i + 1));
+                System.out.println("Please Enter where is the obstacle number " + (i + 1) +
+                        "\nExample X Y");
+                temp = sc.nextLine().split(" ");
+            }
+
+            obstacles[i][0] = Integer.valueOf(temp[0]);
+            obstacles[i][1] = Integer.valueOf(temp[1]);
+        }
+
+        board b = new board(8, knightPos, GoalPos, obstacles);
         b.initalizeBoard();
         System.out.println(b.full[1][5]);
         b.reachGoal();
